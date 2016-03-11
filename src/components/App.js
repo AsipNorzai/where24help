2d6eb4c6818d8b600e1debc9fd978598576b827e
Asep Norzai <asifkhannoorzai@gmail.com>
2016-03-11 22:07:45 +1100

    Just added some smantic html5 tags

--- src/components/App.js
+++ src/components/App.js
@@ -7,11 +7,15 @@ import ItemListDisplay from './ItemListDisplay';
 export default class App extends React.Component {
   render() {
     return (
-      <div className="row">
-          <span className="center">Where24Help</span>
-          <div className=" col s12 m6 offset-m3 l10 offset-l2">
-            {this.props.children}
+      <div>
+        <main>
+          <div className="row">
+              <span className="center">Where24Help</span>
+              <div className=" col s12 m6 offset-m3 l10 offset-l2">
+                {this.props.children}
+              </div>
           </div>
+        </main>
       </div>
     );
   }
